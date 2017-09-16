5be0bbcee528a01e051a743733533e8c1e34e540
Joseph Tran <josephbtran@gmail.com>
2017-09-16 09:55:23 -0700

    Add files via upload

---
+++ cms.js
@@ -0,0 +1,119 @@
+var formidable = require('formidable'),
+    http = require('http'),
+    util = require('util');
+
+$(document).ready(function() {
+  // Gets an optional query string from our url (i.e. ?post_id=23)
+  var url = window.location.search;
+  var postId;
+  // Sets a flag for whether or not we're updating a post to be false initially
+  var updating = false;
+
+  // If we have this section in our url, we pull out the post id from the url
+  // In localhost:8080/cms?post_id=1, postId is 1
+  if (url.indexOf("?post_id=") !== -1) {
+    postId = url.split("=")[1];
+    getPostData(postId);
+  }
+
+  // Getting jQuery references to the post body, title, form, and category select
+  var descriptionInput = $("#description");
+  var titleInput = $("#title");
+  var cmsForm = $("#cms");
+  var postCategorySelect = $("#category");
+  // Giving the postCategorySelect a default value
+  postCategorySelect.val("Personal");
+  // Adding an event listener for when the form is submitted
+  $(cmsForm).on("submit", function handleFormSubmit(event) {
+    event.preventDefault();
+    // Wont submit the post if we are missing a body or a title
+    if (!titleInput.val().trim() || !descriptionInput.val().trim()) {
+      return;
+    }
+    // Constructing a newPost object to hand to the database
+    var newPost = {
+      title: titleInput.val().trim(),
+      description: descriptionInput.val().trim(),
+      category: postCategorySelect.val()
+    };
+
+    console.log(newPost);
+
+    // If we're updating a post run updatePost to update a post
+    // Otherwise run submitPost to create a whole new post
+    if (updating) {
+      newPost.id = postId;
+      updatePost(newPost);
+    }
+    else {
+      submitPost(newPost);
+    }
+  });
+
+  // Submits a new audiopost and brings user to map page upon completion
+  function submitPost(Post) {
+    $.post("/api/posts/", Post, function() {
+      window.location.href = "/map";
+    });
+  }
+
+  // Gets post data for a post if we're editing
+  function getPostData(id) {
+    $.get("/api/posts/" + id, function(data) {
+      console.log(data);
+      if (data) {
+        // If this post exists, prefill our cms forms with its data
+        titleInput.val(data.title);
+        descriptionInput.val(data.description);
+        postCategorySelect.val(data.category);
+        // If we have a post with this id, set a flag for us to know to update the post
+        // when we hit submit
+        updating = true;
+      }
+    });
+  }
+
+  // Update a given post, bring user to the blog page when done
+  function updatePost(post) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/posts",
+      data: post
+    })
+    .done(function() {
+      window.location.href = "/map";
+    });
+  }
+
+
+  /* TEST CASE */
+  var testbox = $("#test");
+
+  function fileUpload(req, res) {
+    if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+      // parse a file upload
+      var form = new formidable.IncomingForm({
+        uploadDir: __dirname + '/assets/images',  // don't forget the __dirname here
+        keepExtensions: true
+      });
+      form.parse(req, function(err, fields, files) {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received upload:\n\n');
+        res.end(util.inspect({fields: fields, files: files}));
+      });
+      return;
+    }
+
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(
+      '<form action="/upload" enctype="multipart/form-data" method="post">'+
+      '<input type="text" name="Audio File"><br>'+
+      '<input type="file" name="upload" multiple="multiple"><br>'+
+      '<input type="submit" value="Upload">'+
+      '</form>'
+    ); 
+  }
+
+
+  
+});
